2b737f2c6f319423e502b01e859560fb95d79623
brightdevs <109676581+brightdevs@users.noreply.github.com>
2022-08-24 12:44:53 -0300

    tooling section is reponsive

--- src/App.tsx
+++ src/App.tsx
@@ -52,6 +52,28 @@ function App() {
           </div>
         </div>
       </section>
+      <section className='tooling-section'>
+        <div className='tooling-section__wrapper'>
+          <div className='tooling-section__wrapper__img'></div>
+          <div className='tooling-section__wrapper__content'>
+            <h1>Free, open, simple</h1>
+            <p>
+              Blogr is a free and open source application backed by a large
+              community of helpful developers. It supports features such as code
+              syntax highlighting, RSS feeds, social media integration,
+              third-party commenting tools, and works seamlessly with Google
+              Analytics. The architecture is clean and is relatively easy to
+              learn.
+            </p>
+            <h1>Powerful tooling</h1>
+            <p>
+              Batteries included. We built a simple and straightforward CLI tool
+              that makes customization and deployment a breeze, but capable of
+              producing even the most complicated sites.
+            </p>
+          </div>
+        </div>
+      </section>
 
       <Overlay isOpen={isOpen} toggle={toggle} />
     </main>
