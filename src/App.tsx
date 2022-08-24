c9e1874d9f651f7ce76c169d67eac328c744a6cb
brightdevs <109676581+brightdevs@users.noreply.github.com>
2022-08-23 23:10:51 -0300

    grid placement created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.scss';
-
+import desktopEditorImg from './images/illustration-editor-desktop.svg';
 import useToggle from './hooks/useToggle';
 import Overlay from './components/overlay.component/overlay';
 import Header from './components/Header.component';
@@ -9,6 +9,31 @@ function App() {
   return (
     <main className='container'>
       <Header isOpen={isOpen} toggle={toggle} />
+      <div className='intro-title '>
+        <h1>Designed for the future</h1>
+      </div>
+      <div className='intro-content '>
+        <h2>Introducing and extensible editor</h2>
+        <p>
+          Blogr features an exceedingly intuitive interface which lets you focus
+          on one thing: creating content. The editor supports management of
+          multiple blogs and allows easy manipulation of embeds such as images,
+          videos, and Markdown. Extensibility with plugins and themes provide
+          easy ways to add functionality or change the looks of a blog.
+        </p>
+        <h2>Robust content management</h2>
+        <p>
+          {' '}
+          Flexible content management enables users to easily move through
+          posts. Increase the usability of your blog by adding customized
+          categories, sections, format, or flow. With this functionality, you’re
+          in full control.
+        </p>
+      </div>
+      <div className='intro-img'>
+        <img src={desktopEditorImg} alt='desktop ilustrator' />
+      </div>
+
       <Overlay isOpen={isOpen} toggle={toggle} />
     </main>
   );
